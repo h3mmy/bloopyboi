628d35a1d1f9458eda96810291770dcf8767cd37
Zee Aslam <zeet6613@gmail.com>
2022-03-05 12:08:46 -0500

    Add var

--- main.go
+++ main.go
@@ -15,10 +15,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	bloopyCommands "gitlab.com/h3mmy/bloopyboi/bot/commands"
 	"gitlab.com/h3mmy/bloopyboi/bot/providers"
 	"gitlab.com/h3mmy/bloopyboi/bot/util"
 
@@ -29,6 +31,10 @@ import (
 // Variables used for command line parameters
 var (
 	Token string
+	guildId string
+	GuildID = &guildId
+	RegisteredCommands []*discordgo.ApplicationCommand
+	RemoveCommands = true
 )
 
 func init() {
@@ -43,6 +49,27 @@ func init() {
 	}
 }
 
+// Register functions as callbacks for varios signatures
+func addHandlers(dg *discordgo.Session) {
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := bloopyCommands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+	dg.AddHandler(messageCreate)
+	dg.AddHandler(directMessageCreate)
+
+	RegisteredCommands = make([]*discordgo.ApplicationCommand, len(bloopyCommands.Commands))
+	for i, v := range bloopyCommands.Commands {
+		// Leaving GuildId empty
+		cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		RegisteredCommands[i] = cmd
+	}
+}
+
 func main() {
 
 	Token = providers.GetBotToken()
@@ -54,9 +81,7 @@ func main() {
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
-	dg.AddHandler(directMessageCreate)
+	addHandlers(dg)
 
 	// Just like the ping pong example, we only care about receiving message
 	// events in this example.
@@ -69,14 +94,38 @@ func main() {
 		return
 	}
 
+
+
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
 
 	// Cleanly close down the Discord session.
-	dg.Close()
+	defer dg.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+
+
+	if RemoveCommands {
+		log.Println("Removing commands...")
+		// // We need to fetch the commands, since deleting requires the command ID.
+		// // We are doing this from the returned commands on line 375, because using
+		// // this will delete all the commands, which might not be desirable, so we
+		// // are deleting only the commands that we added.
+		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+		// if err != nil {
+		// 	log.Fatalf("Could not fetch registered commands: %v", err)
+		// }
+
+		for _, v := range RegisteredCommands {
+			err := dg.ApplicationCommandDelete(dg.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
 }
 
 // Temporary method to check if command is relevant
