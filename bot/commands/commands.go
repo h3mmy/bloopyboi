68725102843aae9290c8f3b9b79c7509bae9dcfd
Zee Aslam <zeet6613@gmail.com>
2022-03-05 11:43:41 -0500

    Add var

--- bot/commands/commands.go
+++ bot/commands/commands.go
@@ -22,26 +22,3 @@ var (
 		},
 	}
 )
-			Name: "inspire",
-			Description: "Summon Inspiration",
-		},
-	}
-	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"inspire": func(s *discordgo.Session, i *discordgo.InteractionCreate)func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "Success",
-				},
-			})
-		},
-	}
-)
-			Name: "inspire",
-			Description: "",
-		},
-	}
-)
-
-	commandHandlers
-)
