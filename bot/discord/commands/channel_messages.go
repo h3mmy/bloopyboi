7e96f8e23fcc52b35fb6417b530fcefb3b61fbf0
Zee Aslam <zeet6613@gmail.com>
2022-08-21 13:56:50 -0400

    Add remaining files

---
+++ bot/discord/commands/channel_messages.go
@@ -0,0 +1,37 @@
+package commands
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"gitlab.com/h3mmy/bloopyboi/bot/util"
+)
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "inspire" {
+		bttp := util.NewBloopyClient()
+		embed := &discordgo.MessageEmbed{
+			Author: &discordgo.MessageEmbedAuthor{},
+			Image: &discordgo.MessageEmbedImage{
+				URL: bttp.Inspiro_api.GetInspiro(),
+			},
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+
+	if m.Content == "Pong!" {
+		s.ChannelMessageSend(m.ChannelID, "-_-")
+	}
+}
---
+++ bot/discord/discord.go
@@ -0,0 +1,102 @@
+package discord
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/sirupsen/logrus"
+	"gitlab.com/h3mmy/bloopyboi/bot/providers"
+	bloopyCommands "gitlab.com/h3mmy/bloopyboi/bot/discord/commands"
+)
+
+// customTimeFormat holds custom time format string.
+const (
+	customTimeFormat = "2006-01-02T15:04:05Z"
+
+	// discordBotMentionRegexFmt supports also nicknames (the exclamation mark).
+	// Read more: https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#how-discord-mentions-work
+	discordBotMentionRegexFmt = "^<@!?%s>"
+)
+
+type DiscordClient struct {
+	botMentionRegex      *regexp.Regexp
+	log                  logrus.FieldLogger
+	botId                string
+	api                  *discordgo.Session
+	registeredCommands   []*discordgo.ApplicationCommand
+}
+
+// Constructs new Discord Client
+func NewDiscordClient(logger logrus.FieldLogger) (*DiscordClient, error) {
+	// Get token
+	token := providers.GetBotToken()
+	botID := providers.GetBotName()
+
+	botMentionRegex, err := regexp.Compile(fmt.Sprintf(discordBotMentionRegexFmt, botID))
+	if err != nil {
+		return nil, fmt.Errorf("while compiling bot mention regex: %w", err)
+	}
+
+	// Create a new Discord session using the provided bot token.
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, fmt.Errorf("Error Creating Discord Session: %w", err)
+	}
+	return &DiscordClient{
+		botId: botID,
+		api: s,
+		log: logger,
+		botMentionRegex: botMentionRegex,
+	}, nil
+}
+
+// Initiates websocket connection with Discord and starts listening
+func (d *DiscordClient) Start(ctx context.Context) error {
+	d.log.Info("Starting Bot")
+	d.api.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := bloopyCommands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+	d.api.AddHandler(bloopyCommands.MessageCreate)
+	d.api.AddHandler(bloopyCommands.DirectMessageCreate)
+
+	d.api.Identify.Intents = discordgo.IntentsGuildMessages
+	// Open a websocket connection to Discord and begin listening.
+	d.log.Info("Opening Websocket Connection")
+	err := d.api.Open()
+	if err != nil {
+		return fmt.Errorf("While opening a connection: %w", err)
+	}
+
+	d.log.Info("Registering Commands")
+	d.registeredCommands = make([]*discordgo.ApplicationCommand, len(bloopyCommands.Commands))
+	for i, v := range bloopyCommands.Commands {
+		// Leaving GuildId empty
+		cmd, err := d.api.ApplicationCommandCreate(d.api.State.User.ID, "", v)
+		if err != nil {
+			d.log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		d.registeredCommands[i] = cmd
+	}
+
+	<- ctx.Done()
+
+	d.log.Info("Removing registered commands...")
+	for _, v := range d.registeredCommands {
+		err := d.api.ApplicationCommandDelete(d.api.State.User.ID, "", v.ID)
+		if err != nil {
+			d.log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+		}
+	}
+
+	d.log.Info("Closing Connection")
+	err = d.api.Close()
+	if err != nil {
+		return fmt.Errorf("while closing connection: %w", err)
+	}
+
+	return nil
+}
