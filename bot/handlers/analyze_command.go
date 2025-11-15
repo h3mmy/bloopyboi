cf82955feda9e4cad395f705b41890559a78328e
Zee Aslam <zeet6613@gmail.com>
2025-11-14 21:43:41 -0500

    fix(analyze-emoji) parsing of discord emoji string

--- bot/handlers/analyze_command.go
+++ bot/handlers/analyze_command.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"slices"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -13,22 +15,29 @@ import (
 )
 
 type AnalyzeEmojiCommand struct {
-	meta              models.BloopyMeta
-	Name              string
-	Description       string
-	guildId           string
-	logger            *zap.Logger
-	roles             []int64
-	imageAnalyzingSvc *services.ImageAnalyzerService
+	meta               models.BloopyMeta
+	Name               string
+	Description        string
+	guildId            string
+	logger             *zap.Logger
+	roles              []int64
+	imageAnalyzingSvc  *services.ImageAnalyzerService
+	discordEmojiRegexp *regexp.Regexp
 }
 
 func NewAnalyzeEmojiCommand(imageAnalyzerSvc *services.ImageAnalyzerService) *AnalyzeEmojiCommand {
+	logger := log.NewZapLogger().Named("analyze_command")
+	rexep, err := regexp.Compile(`<(a|):([A-Za-z0-9_~]+):([0-9]{18,20})>`)
+	if err != nil {
+		logger.Error("error compiling regexp", zap.Error(err))
+	}
 	return &AnalyzeEmojiCommand{
-		meta:              models.NewBloopyMeta(),
-		logger:            log.NewZapLogger().Named("analyze_command"),
-		Name:              string(Analyze),
-		Description:       "Analyze an emoji using an ML model",
-		imageAnalyzingSvc: imageAnalyzerSvc,
+		meta:               models.NewBloopyMeta(),
+		logger:             logger,
+		Name:               string(Analyze),
+		Description:        "Analyze an emoji using an ML model",
+		imageAnalyzingSvc:  imageAnalyzerSvc,
+		discordEmojiRegexp: rexep,
 	}
 }
 
@@ -71,59 +80,69 @@ func (c *AnalyzeEmojiCommand) GetMessageComponentHandlers() map[string]func(s *d
 func (c *AnalyzeEmojiCommand) GetAppCommandHandler() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		emojiArg := i.ApplicationCommandData().Options[0].StringValue()
-		if (strings.HasPrefix(emojiArg, "<:") || strings.HasPrefix(emojiArg, "<a:")) && strings.HasSuffix(emojiArg, ">") {
-			trimmed := strings.Trim(emojiArg, "<:>")
-			parts := strings.Split(trimmed, ":")
-			if len(parts) == 2 || len(parts) == 3 {
-				var emojiID string
-				animated := false
-				if len(parts) == 3 {
-					emojiID = parts[1]
-					animated = true
-				} else {
-					emojiID = parts[2]
-				}
+		// examples of expected values
+		// <:litolshy:1236822872811634699>
+		// <a:batty_giggle:1071603174760251414>
 
-				extension := "png" // Could also be jpg or webp
-				if animated {
-					extension = "gif"
-				}
+		matchSlice := c.discordEmojiRegexp.FindStringSubmatch(emojiArg)
+		c.logger.Debug("matched a discord emoji", zap.Any("match_slice", matchSlice))
 
-				emojiURL := fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.%s", emojiID, extension)
+		if len(matchSlice) == 0 || len(matchSlice) == 1 {
+			response := fmt.Sprintf("🙁 failed to parse emojiID from string: %s\nGot matchSlice:%+v", emojiArg, matchSlice)
 
-				analysis, err := c.imageAnalyzingSvc.AnalyzeImageFromURL(context.Background(), emojiURL)
-				if err != nil {
-					c.logger.Error("error analyzing image", zap.Error(err))
-					errR := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-						Type: discordgo.InteractionResponseChannelMessageWithSource,
-						Data: &discordgo.InteractionResponseData{
-							Content: fmt.Sprintf("Failed to analyze emoji: %v", err),
-						},
-					})
-					if errR != nil {
-						c.logger.Error("error responding to interaction", zap.Error(errR))
-					}
-					return
-				}
-
-				// Format and send the results
-				response := fmt.Sprintf("Analysis for %s:\nKeywords: %s\nSafeSearchAnnotation: %s",
-					emojiArg,
-					strings.Join(analysis.GetKeywordsSortedByScore(), ", "),
-					PrintJSON(analysis.SafeSearchAnalysis))
-
-					errR := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: response,
-					},
-				})
-				if errR != nil {
-					c.logger.Error("error responding to interaction", zap.Error(errR))
-				}
-				return
+			errR := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: response,
+				},
+			})
+			if errR != nil {
+				c.logger.Error("error responding to interaction", zap.Error(errR))
 			}
 		}
+		emojiID := matchSlice[len(matchSlice)-1]
+		animated := slices.Contains(matchSlice, "a")
+		extension := "png" // Could also be jpg or webp
+		if animated {
+			extension = "gif"
+		}
+
+		emojiURL := fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.%s", emojiID, extension)
+
+		analysis, err := c.imageAnalyzingSvc.AnalyzeImageFromURL(context.Background(), emojiURL)
+		if err != nil {
+			c.logger.Error("error analyzing image", zap.Error(err))
+			errR := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Failed to analyze emoji: %v", err),
+				},
+			})
+			if errR != nil {
+				c.logger.Error("error responding to interaction", zap.Error(errR))
+			}
+			return
+		} else {
+			c.logger.Debug("Got analysis results", zap.Any("analysis", analysis))
+		}
+
+
+		// Format and send the results
+		response := fmt.Sprintf("Analysis for %s:\nKeywords: %s\nSafeSearchAnnotation: %s",
+			emojiArg,
+			strings.Join(analysis.GetKeywordsSortedByScore(), ", "),
+			PrintJSON(analysis.SafeSearchAnalysis))
+
+		errR := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: response,
+			},
+		})
+		if errR != nil {
+			c.logger.Error("error responding to interaction", zap.Error(errR))
+		}
+
 	}
 }
 
