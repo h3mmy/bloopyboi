9af1d8bceeecc105a0e774cf175475bdcf357047
Zee Aslam <zeet6613@gmail.com>
2025-11-27 15:23:42 -0500

    refactor emoji handler to extract common logic
    Signed-off-by: Zee Aslam <zeet6613@gmail.com>

--- bot/handlers/analyze_guild_emojis.go
+++ bot/handlers/analyze_guild_emojis.go
@@ -5,14 +5,9 @@ import (
 	"context"
 	"fmt"
 
-	"entgo.io/ent/dialect/sql"
 	"github.com/bwmarrin/discordgo"
 	"github.com/h3mmy/bloopyboi/bot/services"
-	"github.com/h3mmy/bloopyboi/ent"
-	"github.com/h3mmy/bloopyboi/ent/emojikeywordscore"
-	"github.com/h3mmy/bloopyboi/ent/keyword"
 	"github.com/h3mmy/bloopyboi/internal/models"
-	"github.com/h3mmy/bloopyboi/pkg/database"
 	"github.com/h3mmy/bloopyboi/pkg/logs"
 	"go.uber.org/zap"
 )
@@ -21,15 +16,15 @@ type ImageAnalysisHandler struct {
 	meta             models.BloopyMeta
 	logger           *zap.Logger
 	imageAnalyzerSvc *services.ImageAnalyzerService
-	db               *ent.Client
+	discordSvc       *services.DiscordService
 }
 
-func NewImageAnalysisHandler(imageAnalyzerSvc *services.ImageAnalyzerService, db *ent.Client) *ImageAnalysisHandler {
+func NewImageAnalysisHandler(imageAnalyzerSvc *services.ImageAnalyzerService, discordSvc *services.DiscordService) *ImageAnalysisHandler {
 	return &ImageAnalysisHandler{
 		meta:             models.NewBloopyMeta(),
 		logger:           logs.NewZapLogger().Named("image_analysis_handler"),
 		imageAnalyzerSvc: imageAnalyzerSvc,
-		db:               db,
+		discordSvc:       discordSvc,
 	}
 }
 
@@ -47,100 +42,13 @@ func (i *ImageAnalysisHandler) ProcessGuildEmojis(ctx context.Context, emoji []*
 			continue
 		}
 
-		err = i.SaveDiscordEmojiWithAnalysis(ctx, analysis)
+		err = i.discordSvc.SaveDiscordEmojiWithAnalysis(ctx, analysis)
 		if err != nil {
 			logr.Error("failed to persist emoji analysis", zap.String("emoji_id", e.ID), zap.Error(err))
 			continue
 		}
 	}
 	return nil
-}
-
-func (i *ImageAnalysisHandler) SaveDiscordEmojiWithAnalysis(ctx context.Context, analysis *models.DiscordEmojiAnalysisResult) error {
-	logr := i.logger.With(zap.Any("context", ctx))
-	logr.Debug("saving discord emoji with analysis", zap.Any("analysis", analysis))
-	tx, err := i.db.Tx(ctx)
-	if err != nil {
-		return fmt.Errorf("starting a transaction: %w", err)
-	}
-
-	emjID, err := tx.Emoji.
-		Create().
-		SetEmojiID(analysis.Emoji.ID).
-		SetAnimated(analysis.Emoji.Animated).
-		SetName(analysis.Emoji.Name).
-		SetImageURI(analysis.ImageURI).
-		SetAdultLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Adult)).
-		SetRacyLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Racy)).
-		SetSpoofLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Spoof)).
-		SetViolenceLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Violence)).
-		SetMedicalLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Medical)).
-		OnConflict(sql.ConflictColumns("emoji_id")).
-		UpdateNewValues().
-		ID(ctx)
-
-	if err != nil {
-		logr.Error("failed to create emoji", zap.String("emoji_id", analysis.Emoji.ID), zap.Error(err))
-		return database.Rollback(tx, err)
-	}
-
-	keywordBuilders := make([]*ent.KeywordCreate, len(analysis.AnalysisResult.Labels))
-	keywordScoreMap := make(map[string]models.EntityLabelAnnotation)
-	keywordList := make([]string, len(analysis.AnalysisResult.Labels))
-
-	for i, keyword := range analysis.AnalysisResult.Labels {
-		keywordBuilders[i] = tx.Keyword.Create().
-			SetKeyword(keyword.Description).
-			AddEmojiIDs(emjID)
-		keywordScoreMap[keyword.Description] = keyword
-		keywordList[i] = keyword.Description
-	}
-
-	err = tx.Keyword.CreateBulk(
-		keywordBuilders...,
-	).OnConflictColumns(keyword.FieldKeyword).
-		UpdateNewValues().
-		Exec(ctx)
-
-	if err != nil {
-		logr.Error("failed to upsert keywords", zap.String("emoji_id", analysis.Emoji.ID), zap.Error(err))
-		return database.Rollback(tx, err)
-	}
-
-	keywords, err := tx.Keyword.
-		Query().
-		Where(keyword.KeywordIn(keywordList...)).
-		All(ctx)
-
-	if err != nil {
-		logr.Error("failed to query upserted keywords", zap.Error(err))
-		return database.Rollback(tx, err)
-	}
-	keywordScoreBuilders := make([]*ent.EmojiKeywordScoreCreate, len(keywords))
-	for i, keyw := range keywords {
-		keywordScoreBuilders[i] = tx.EmojiKeywordScore.Create().
-			SetEmojiID(emjID).
-			SetKeywordID(keyw.ID).
-			SetScore(keywordScoreMap[keyw.Keyword].Score).
-			SetTopicality(keywordScoreMap[keyw.Keyword].Topicality)
-	}
-
-	err = tx.EmojiKeywordScore.CreateBulk(
-		keywordScoreBuilders...,
-	).
-		OnConflictColumns(
-			emojikeywordscore.FieldEmojiID,
-			emojikeywordscore.FieldKeywordID,
-		).
-		UpdateNewValues().
-		Exec(ctx)
-
-	if err != nil {
-		logr.Error("failed to upsert keyword scores", zap.Error(err))
-		return database.Rollback(tx, err)
-	}
-
-	return tx.Commit()
 }
 
 func (i *ImageAnalysisHandler) AnalyzeDiscordEmoji(ctx context.Context, emoji *discordgo.Emoji) (analysis *models.DiscordEmojiAnalysisResult, err error) {
--- bot/services/discord_service.go
+++ bot/services/discord_service.go
@@ -13,6 +13,8 @@ import (
 	"github.com/h3mmy/bloopyboi/ent/discordmessage"
 	"github.com/h3mmy/bloopyboi/ent/discordmessagereaction"
 	"github.com/h3mmy/bloopyboi/ent/discorduser"
+	"github.com/h3mmy/bloopyboi/ent/emojikeywordscore"
+	"github.com/h3mmy/bloopyboi/ent/keyword"
 	"github.com/h3mmy/bloopyboi/internal/discord"
 	"github.com/h3mmy/bloopyboi/internal/models"
 	"github.com/h3mmy/bloopyboi/pkg/config"
@@ -553,45 +555,91 @@ func (d *DiscordService) GetSavedGuild(ctx context.Context, discordGuildId strin
 	return discordGuild, nil
 }
 
-func (d *DiscordService) IngestGuildEmojis(ctx context.Context, guildID string) error {
-	// if d.imageAnalyzerSvc == nil {
-	// 	return errors.New("image analyzer is not available")
-	// }
-
-	emojis, err := d.discordSession.GuildEmojis(guildID)
+func (d *DiscordService) SaveDiscordEmojiWithAnalysis(ctx context.Context, analysis *models.DiscordEmojiAnalysisResult) error {
+	logr := d.logger.With(zap.Any("context", ctx))
+	logr.Debug("saving discord emoji with analysis", zap.Any("analysis", analysis))
+	tx, err := d.db.Tx(ctx)
 	if err != nil {
-		return fmt.Errorf("could not get emojis for guild %s: %w", guildID, err)
+		return fmt.Errorf("starting a transaction: %w", err)
 	}
 
-	for _, emoji := range emojis {
-		imageURL := fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.png", emoji.ID)
-		if emoji.Animated {
-			imageURL = fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.gif", emoji.ID)
-		}
+	emjID, err := tx.Emoji.
+		Create().
+		SetEmojiID(analysis.Emoji.ID).
+		SetAnimated(analysis.Emoji.Animated).
+		SetName(analysis.Emoji.Name).
+		SetImageURI(analysis.ImageURI).
+		SetAdultLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Adult)).
+		SetRacyLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Racy)).
+		SetSpoofLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Spoof)).
+		SetViolenceLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Violence)).
+		SetMedicalLikelihood(int(analysis.AnalysisResult.SafeSearchAnalysis.Medical)).
+		OnConflict(sql.ConflictColumns("emoji_id")).
+		UpdateNewValues().
+		ID(ctx)
 
-		analysis, err := d.imageAnalyzerSvc.AnalyzeImageFromURL(ctx, imageURL)
-		if err != nil {
-			d.logger.Error("failed to analyze emoji image", zap.String("emoji_id", emoji.ID), zap.Error(err))
-			continue
-		}
-
-		d.logger.Info("analyzed emoji", zap.String("emoji_id", emoji.ID), zap.Strings("keywords", analysis.GetKeywords()))
-
-		if d.dbEnabled {
-			err := d.db.Emoji.Create().
-				SetEmojiID(emoji.ID).
-				SetName(emoji.Name).
-				SetAnimated(emoji.Animated).
-				OnConflict(sql.ConflictColumns("emoji_id")).
-				UpdateNewValues().
-				Exec(ctx)
-			if err != nil {
-				d.logger.Error("failed to save emoji to db", zap.String("emoji_id", emoji.ID), zap.Error(err))
-			}
-		}
+	if err != nil {
+		logr.Error("failed to create emoji", zap.String("emoji_id", analysis.Emoji.ID), zap.Error(err))
+		return database.Rollback(tx, err)
 	}
 
-	return nil
+	keywordBuilders := make([]*ent.KeywordCreate, len(analysis.AnalysisResult.Labels))
+	keywordScoreMap := make(map[string]models.EntityLabelAnnotation)
+	keywordList := make([]string, len(analysis.AnalysisResult.Labels))
+
+	for i, keyword := range analysis.AnalysisResult.Labels {
+		keywordBuilders[i] = tx.Keyword.Create().
+			SetKeyword(keyword.Description).
+			AddEmojiIDs(emjID)
+		keywordScoreMap[keyword.Description] = keyword
+		keywordList[i] = keyword.Description
+	}
+
+	err = tx.Keyword.CreateBulk(
+		keywordBuilders...,
+	).OnConflictColumns(keyword.FieldKeyword).
+		UpdateNewValues().
+		Exec(ctx)
+
+	if err != nil {
+		logr.Error("failed to upsert keywords", zap.String("emoji_id", analysis.Emoji.ID), zap.Error(err))
+		return database.Rollback(tx, err)
+	}
+
+	keywords, err := tx.Keyword.
+		Query().
+		Where(keyword.KeywordIn(keywordList...)).
+		All(ctx)
+
+	if err != nil {
+		logr.Error("failed to query upserted keywords", zap.Error(err))
+		return database.Rollback(tx, err)
+	}
+	keywordScoreBuilders := make([]*ent.EmojiKeywordScoreCreate, len(keywords))
+	for i, keyw := range keywords {
+		keywordScoreBuilders[i] = tx.EmojiKeywordScore.Create().
+			SetEmojiID(emjID).
+			SetKeywordID(keyw.ID).
+			SetScore(keywordScoreMap[keyw.Keyword].Score).
+			SetTopicality(keywordScoreMap[keyw.Keyword].Topicality)
+	}
+
+	err = tx.EmojiKeywordScore.CreateBulk(
+		keywordScoreBuilders...,
+	).
+		OnConflictColumns(
+			emojikeywordscore.FieldEmojiID,
+			emojikeywordscore.FieldKeywordID,
+		).
+		UpdateNewValues().
+		Exec(ctx)
+
+	if err != nil {
+		logr.Error("failed to upsert keyword scores", zap.Error(err))
+		return database.Rollback(tx, err)
+	}
+
+	return tx.Commit()
 }
 
 func (d *DiscordService) UpdateAppRoleMetadata(ctx context.Context, cfg *oauth2.Config) error {
